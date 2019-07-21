bcb50c0397e2c3c82e7852a8a050929fff90eb50
Ike Njoku <ikeenjoku@gmail.com>
2019-07-21 17:37:16 +0100

    define app routes

---
+++ src/routes/appRoutes.js
@@ -0,0 +1,14 @@
+import HomePage from "../components/home";
+import MainApp from "../components/main";
+import LoginPage from "../components/login";
+import ResetPasswordPage from "../components/resetPassword";
+import NoMatch from "../components/noMatch";
+
+export default [
+  { path: "/", component: HomePage, exact: true },
+  { path: "/app", component: MainApp },
+  { path: "/login", component: LoginPage },
+  { path: "/password_reset", component: ResetPasswordPage },
+  { component: NoMatch },
+];
+
