5063152d7b76c58c2e7d558ad28e088a686c2f4b
Ike Njoku <ikeenjoku@gmail.com>
2019-07-22 18:28:15 +0100

    design landing page

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './home.css';
 
 class Home extends Component {
   render() {
     return (
-      <div> Home Page </div>
+      <div>
+        <header>
+          <img className='logo' alt='triple-7-logo' src={require('../../assets/tripleLogoWeb.png')} />
+        </header>
+        <div className="login-body login-bg">
+          <div className="login-content">
+            <p className="welcome-text">Welcome to 3riple 7even</p>
+            <p className="admin-text">Admin</p>
+            <Link
+              to="/login"
+            >
+              <button className="btn-login">Login</button>
+            </Link>
+          </div>
+        </div>
+      </div>
     );
   }
 }
