d527d6e3c280f927f0774426e91668f6378ee2fc
Ike Njoku <ikeenjoku@gmail.com>
2019-08-08 17:01:38 +0100

    add development url

--- src/redux/axiosConfig.js
+++ src/redux/axiosConfig.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-let api = 'https://triple7-eating-house.herokuapp.com/api/v1';
+let api = process.env.NODE_ENV === 'production' ?
+  'https://triple7-eating-house.herokuapp.com/api/v1' :
+  'http://localhost:7777/api/v1';
 
 export default axios.create({
   baseURL: api,
