f69aa3f34074e16cb04c64275b252b31f966f5f6
Ike Njoku <ikeenjoku@gmail.com>
2019-08-09 15:50:06 +0100

    src/components/app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import jwtDecode from 'jwt-decode';
 import { Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../redux/configureStore';
 import history from './history';
 import appRoutes from '../routes/appRoutes';
+import { setAuthorizationToken } from '../utils/authHelpers';
+import { login_user_success } from '../redux/actions/authActions';
+
+if (localStorage.triple7) {
+  const decoded = jwtDecode(localStorage.triple7);
+  const { user } = decoded;
+  setAuthorizationToken(localStorage.triple7);
+  store.dispatch(login_user_success({
+      id: user._id,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      email: user.email,
+      phone: user.phone,
+      gender: user.gender,
+      role: user.role,
+    })
+  );
+}
 
 const routes = appRoutes.map((appRoute,key) =>
   <Route
