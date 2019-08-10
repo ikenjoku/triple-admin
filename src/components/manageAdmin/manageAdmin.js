64589dc19f51ef1258173319e789fc858ed87095
Ike Njoku <ikeenjoku@gmail.com>
2019-08-10 18:12:41 +0100

    add action handlers

--- src/components/manageAdmin/manageAdmin.js
+++ src/components/manageAdmin/manageAdmin.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import { fetchStaff, addStaff, removeStaff } from '../../redux/actions/adminActions';
 import './manageAdmin.css';
 
 export class ManageAdmin extends Component {
@@ -8,14 +9,11 @@ export class ManageAdmin extends Component {
     data: {
       email: '',
     },
-    staff: [
-      { firstname: 'Lara', lastname: 'Daniels', email: 'laracroft@gmail.com' },
-      { firstname: 'Lara', lastname: 'Daniels', email: 'laracroft@gmail.com' },
-      { firstname: 'Lara', lastname: 'Daniels', email: 'laracroft@gmail.com' },
-      { firstname: 'Lara', lastname: 'Daniels', email: 'laracroft@gmail.com' },
-      { firstname: 'Lara', lastname: 'Daniels', email: 'laracroft@gmail.com' },
-      { firstname: 'Lara', lastname: 'Daniels', email: 'laracroft@gmail.com' },
-    ],
+    error: '',
+  }
+
+  componentDidMount() {
+    this.props.fetchStaff();
   }
 
   onFormInput = (event) => {
@@ -26,24 +24,46 @@ export class ManageAdmin extends Component {
   }
 
   renderStaffMember = (member, index) => {
+    const { removeStaff } = this.props;
     return (
       <div key={index} className="contain-staff-member">
         <h3>{`${member.firstname} ${member.lastname} ${member.email}`}</h3>
         <span style={{ marginLeft: '25px' }}>
-          <FontAwesomeIcon icon="window-close" className="remove-btn" />
+          <FontAwesomeIcon onClick={() => removeStaff(member._id)} icon="window-close" className="remove-btn" />
         </span>
       </div>
     );
   }
 
+  handleAddStaffMember = (event) => {
+    event.preventDefault();
+    const { data: { email } } = this.state;
+    // eslint-disable-next-line no-useless-escape
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!email) {
+      this.setState(() => ({ error: 'Please provide a registered email address' }));
+    } else if (!re.test(email)) {
+      this.setState(() => ({ error: 'Please provide a valid email address' }));
+    } else {
+      this.setState(() => ({ error: '' }));
+      this.props.addStaff({email})
+    }
+  }
+
   render() {
+    const { staff } = this.props;
     return (
       <div>
         <p className="page-title">Manage Staff Members</p>
         <div className="contain-manage-admin">
           <div className="contain-form">
-            <form id="login-form" className="admin-form-wrapper">
+            <form id="login-form" className="admin-form-wrapper" onSubmit={this.handleAddStaffMember}>
               <h2 className="form-title">Add a new staff member</h2>
+              {this.state.error &&
+                <div className="alert alert-danger">
+                  {this.state.error}
+                </div>}
               <div>
                 <input
                   className="form-control staff-form"
@@ -65,7 +85,7 @@ export class ManageAdmin extends Component {
             </form>
             <div className="contain-list admin-form-wrapper">
               {
-                this.state.staff.map(this.renderStaffMember)
+                staff.map(this.renderStaffMember)
               }
             </div>
           </div>
@@ -75,4 +95,10 @@ export class ManageAdmin extends Component {
   }
 }
 
-export default ManageAdmin;
+const mapStateToProps = ({ adminReducer }) => ({
+  staff: adminReducer.staff,
+  isLoading: adminReducer.isLoading,
+  error: adminReducer.error,
+});
+
+export default connect(mapStateToProps, { fetchStaff, addStaff, removeStaff })(ManageAdmin);
