75475b9b33ba74a02cd1cec0fe1e3f24eaa58d59
Ike Njoku <ikeenjoku@gmail.com>
2019-08-24 22:48:25 +0100

    add meal reducer

--- src/redux/reducers/mealReducer.js
+++ src/redux/reducers/mealReducer.js
@@ -5,6 +5,11 @@ import {
   ADD_MEAL,
   ADD_MEAL_SUCCESS,
   ADD_MEAL_FAILURE,
+  EDIT_MEAL,
+  EDIT_MEAL_FAILURE,
+  DELETE_MEAL,
+  DELETE_MEAL_SUCCESS,
+  DELETE_MEAL_FAILURE,
 } from '../actionTypes';
 import initialState from './initialState';
 
@@ -21,6 +26,17 @@ const mealReducer = (state = initialState.mealReducer, action) => {
     case ADD_MEAL_SUCCESS:
       return { ...state, isLoading: false, menu: [...state.menu, action.meal], error: null };
     case ADD_MEAL_FAILURE:
+      return { ...state, isLoading: false, error: action.error };
+    case EDIT_MEAL:
+      return { ...state, isLoading: action.isLoading };
+    case EDIT_MEAL_FAILURE:
+      return { ...state, isLoading: false, error: action.error };
+    case DELETE_MEAL:
+      return { ...state, isLoading: action.isLoading };
+    case DELETE_MEAL_SUCCESS:
+      const newMenu = state.menu.filter(meal => meal._id !== action.mealId)
+      return { ...state, isLoading: false, menu: newMenu, error: null };
+    case DELETE_MEAL_FAILURE:
       return { ...state, isLoading: false, error: action.error };
     default:
       return state;
