a1aa31b172a4cf9d1570f7010bdec11e4199a89e
Ike Njoku <ikeenjoku@gmail.com>
2019-08-24 14:10:11 +0100

    add meal reducer

--- src/redux/reducers/mealReducer.js
+++ src/redux/reducers/mealReducer.js
@@ -2,16 +2,25 @@ import {
   FETCH_MENU,
   FETCH_MENU_SUCCESS,
   FETCH_MENU_FAILURE,
+  ADD_MEAL,
+  ADD_MEAL_SUCCESS,
+  ADD_MEAL_FAILURE,
 } from '../actionTypes';
 import initialState from './initialState';
 
 const mealReducer = (state = initialState.mealReducer, action) => {
   switch (action.type) {
     case FETCH_MENU:
-      return { ...state, isLoading: true };
+      return { ...state, isLoading: action.isLoading };
     case FETCH_MENU_SUCCESS:
       return { ...state, isLoading: false, menu: action.menu, error: null };
     case FETCH_MENU_FAILURE:
+      return { ...state, isLoading: false, error: action.error };
+    case ADD_MEAL:
+      return { ...state, isLoading: action.isLoading };
+    case ADD_MEAL_SUCCESS:
+      return { ...state, isLoading: false, menu: [...state.menu, action.meal], error: null };
+    case ADD_MEAL_FAILURE:
       return { ...state, isLoading: false, error: action.error };
     default:
       return state;
