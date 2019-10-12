d5dd9896ca32291117c0367081559d790ff9a972
Ike Njoku <ikeenjoku@gmail.com>
2019-10-12 14:06:25 +0100

    update highlight reducer

--- src/redux/reducers/highlightReducer.js
+++ src/redux/reducers/highlightReducer.js
@@ -2,11 +2,20 @@ import {
   ADD_HIGHLIGHTS,
   ADD_HIGHLIGHTS_SUCCESS,
   ADD_HIGHLIGHTS_FAILURE,
+  FETCH_HIGHLIGHTS,
+  FETCH_HIGHLIGHTS_SUCCESS,
+  FETCH_HIGHLIGHTS_FAILURE,
 } from '../actionTypes';
 import initialState from './initialState';
 
 const highlightReducer = (state = initialState.highlightReducer, action) => {
   switch (action.type) {
+    case FETCH_HIGHLIGHTS:
+      return { ...state, isLoading: action.isLoading };
+    case FETCH_HIGHLIGHTS_SUCCESS:
+      return { ...state, isLoading: false, highlights: action.highlights, error: null };
+    case FETCH_HIGHLIGHTS_FAILURE:
+      return { ...state, isLoading: false, error: action.error };
     case ADD_HIGHLIGHTS:
       return { ...state, isLoading: action.isLoading };
     case ADD_HIGHLIGHTS_SUCCESS:
